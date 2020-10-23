a648b0ccca1bdc3015d7e81ab26d753e4c16d0bb
Stratfab <fabsiegele@gmail.com>
2020-10-23 09:22:16 +0200

    envoi Class Niveaux

---
+++ GameProject/src/com/company/Niveaux.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Niveaux {
+
+    public static void niveaux(Object o){
+        // write your code here
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Choix du niveau");
+
+
+        System.out.println("- A : Niveau facile");
+        System.out.println("- B : Niveau moyen");
+        System.out.println("- C : Niveau difficile");
+        char choix=scan.next().charAt(0);            //ligne input de caracteres
+
+        switch (choix) {
+            case 'a':
+                System.out.println("Niveau facile");
+                break;
+            case 'b':
+                System.out.println("Niveau moyen");
+                //lance le niveau moyen
+                break;
+            case 'c':
+                System.out.println("Niveau difficile");
+                //lance le niveau difficile
+                break;
+            default:// si autre lettre
+                System.out.println("Refaites votre choix");
+                System.out.println();
+                niveaux(null);
+
+
+
+        }
+
+
+
+    }
+
+
+}
